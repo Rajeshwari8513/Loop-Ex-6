2da1f2b631f700dfedf3cc045b8cf3dc7951330d
Rajeshwari8513 <105580174+Rajeshwari8513@users.noreply.github.com>
2023-08-17 19:31:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Loop-Ex-6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-jgzdxe)
---
+++ index.js
@@ -0,0 +1,6 @@
+let number = 5;
+
+for(let i=0; i<=10; i++){
+    result = i*number;
+    console.log(`${number} * ${i} = ${result}`);  
+}
